f5737f76e548ac7c4be2c66537916f8957461ddc
DilmurodZz <161562453+DilmurodZz@users.noreply.github.com>
2025-02-14 16:24:50 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,7 @@
+def filterGreaterThan(threshold, **kwargs):
+    filtered = {}
+    for A, B in kwargs.items():
+        if isinstance(B, (int, float)) and B > threshold:
+            filtered[A] = B
+    return filtered
 
