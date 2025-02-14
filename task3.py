2d8c429e4f2cf26e5ecbf381350df2f78d24d157
DilmurodZz <161562453+DilmurodZz@users.noreply.github.com>
2025-02-14 16:40:55 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,7 +1,11 @@
-def filterGreaterThan(threshold, **kwargs):
-    filtered = {}
-    for A, B in kwargs.items():
-        if isinstance(B, (int, float)) and B > threshold:
-            filtered[A] = B
-    return filtered
+import time
 
+def decorator_1(func):
+    def wrapper(*white, **black):
+        stime = time.time()
+        result = func(*white, **black)
+        endtime = time.time()
+        extime = endtime - stime
+        print(f"{func.__name__} call executed in {extime:.4f} sec")
+        return result
+    return wrapper
